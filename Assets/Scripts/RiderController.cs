05833cd85bf9912ccc1fdf15590a46dade04417e
Kipid <syafiq.dosuki@gmail.com>
2018-02-19 19:42:17 +0800

    Initial commit

---
+++ Assets/Scripts/RiderController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RiderController : MonoBehaviour {
+
+	bool Action = false;
+	bool isGrounded = false;
+
+	public Rigidbody2D rb;
+
+	public float speed = 20f;
+	public float rotationSpeed = 7f;
+
+	private void Update()
+	{
+	
+		if (Input.GetButtonDown ("Jump")) {
+		
+			Action = true;
+		}
+
+		if (Input.GetButtonUp ("Jump")) {
+		
+			Action = false;
+		}
+	}
+
+	private void FixedUpdate() 
+	{
+		if (Action == true) 
+		{
+			if (isGrounded) {	
+				rb.AddForce (transform.right * speed * Time.fixedDeltaTime * 100f, ForceMode2D.Force);
+			}
+			else
+			{
+				rb.AddTorque (rotationSpeed * Time.fixedDeltaTime * 100f, ForceMode2D.Force);
+			}
+		}
+	
+	}
+
+	private void OnCollisionEnter2D()
+	{
+		isGrounded = true;
+	}
+
+	private void OnCollisionExit2D()
+	{
+		isGrounded = false;
+	}
+}
